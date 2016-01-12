c9057d6ddcb0acf4a8ce956ffeb27117eae0a58d
David Montgomery <davidmontgomery@gmail.com>
2016-01-13 04:02:01 +0800

    update

--- recipes/druid_templates.rb
+++ recipes/druid_templates.rb
@@ -31,9 +31,9 @@ ruleTable = druid[node.chef_environment]['ruleTable']
 data_bag("server_data_bag")
 zookeeper_server = data_bag_item("server_data_bag", "zookeeper")
 if cluster_slug=="nocluster"
-  subdomain = "#{server_type}-#{datacenter}-#{environment}-#{location}-#{slug}"
+  subdomain = "zookeeper-#{datacenter}-#{environment}-#{location}-#{slug}"
 else
-  subdomain = "#{cluster_slug}-#{server_type}-#{datacenter}-#{environment}-#{location}-#{slug}"
+  subdomain = "#{cluster_slug}-zookeeper-#{datacenter}-#{environment}-#{location}-#{slug}"
 end
 required_count = zookeeper_server[datacenter][environment][location][cluster_slug]['required_count']
 full_domain = "#{subdomain}.#{domain}"
