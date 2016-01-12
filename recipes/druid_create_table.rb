9f4ff453acc360a21d2ada0be7f9a756f1046849
David Montgomery <davidmontgomery@gmail.com>
2016-01-13 03:37:36 +0800

    update

--- recipes/druid_create_table.rb
+++ recipes/druid_create_table.rb
@@ -5,7 +5,7 @@ server_type = node.name.split('-')[3]
 slug = node.name.split('-')[4] 
 cluster_slug = File.read("/var/cluster_slug.txt")
 cluster_slug = cluster_slug.gsub(/\n/, "") 
-
+cluster_slug_druid = "druid"
 data_bag("meta_data_bag")
 git = data_bag_item("meta_data_bag", "git")
 aws = data_bag_item("meta_data_bag", "aws")
@@ -13,13 +13,15 @@ domain = aws[node.chef_environment]["route53"]["domain"]
 
 data_bag("server_data_bag")
 mysql_server = data_bag_item("server_data_bag", server_type)
-mysql_password = mysql_server[datacenter][environment][location][cluster_slug]['meta']['password']
-mysql_username = mysql_server[datacenter][environment][location][cluster_slug]['meta']['username']
+mysql_password = mysql_server[datacenter][environment][location][cluster_slug_druid]['meta']['password']
+mysql_username = mysql_server[datacenter][environment][location][cluster_slug_druid]['meta']['username']
 mysql_database = "druid"
 mysql_host = "primary-druid-mysql-#{datacenter}-#{environment}-#{location}-#{slug}.#{domain}"
 
-
-
+=begin
+CREATE SCHEMA IF NOT EXISTS druid;
+ALTER DATABASE druid charset=utf8;
+=end
 
 bash "install_druid_table" do
   user "root"
@@ -27,7 +29,7 @@ bash "install_druid_table" do
   code <<-EOH
     echo 'CREATE SCHEMA IF NOT EXISTS #{mysql_database};' | mysql -u root -p#{mysql_password}
     echo 'ALTER DATABASE #{mysql_database} charset=utf8;' | mysql -u root -p#{mysql_password}
-    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '#{mysql_password}';" | mysql -u root -p#{mysql_password}
+    #echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '#{mysql_password}';" | mysql -u root -p#{mysql_password}
     touch #{Chef::Config[:file_cache_path]}/druid_createtable.lock
   EOH
   action :run
